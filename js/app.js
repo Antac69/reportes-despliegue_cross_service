8d954e1cd70b90c0ceafb85d9788315236735e8d
antac69 <angello_04_09@hotmail.com>
2022-02-18 08:38:52 -0500

    funcionalidad al 15%, agrege funciones reutilizables, limite, deselecionar el texto y algunas animaciones css

--- js/app.js
+++ js/app.js
@@ -1,33 +1,37 @@
 /* variables */
-const movimiento = document.getElementById('movimiento-forms');
-const movimiento_botones = document.getElementById('movimiento-btns');
+const move_forms = document.getElementById('movimiento-forms');
+const move_btns = document.getElementById('movimiento-btns');
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
-const btn_retro = document.getElementById('btn-retro');
+const btn_back = document.getElementById('btn-retro');
 const btn_edit = document.getElementById('btn-editar');
 const btn_save = document.getElementById('btn-save');
-const reporte = document.getElementById('reporte');
-
+const report_area = document.getElementById('reporte');
 let pag = 0;
 
+/* funciones  reutilizables*/
+const limitNext =()=> pag <= 1 ? pag++ :  pag = 2;
+const limitBack =()=> pag > 0 ? pag-- :  pag = 0;
+const move =()=>{
+    move_forms.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
+    move_btns.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
+}
 const nextPag=()=>{
-    pag++;
-    movimiento.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
-    movimiento_botones.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
+    limitNext()
+    move()
 }
 const backPag=()=>{
-    pag--;
-    movimiento.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
-    movimiento_botones.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
+    limitBack()
+    move()
 }
-const copiarPortapapeles = async()=> {
-    await reporte.select()
-    await document.execCommand('copy')
+const copiarPortapapeles =()=> {
+    report_area.select()
+    document.execCommand('copy')
+    btn_save.focus()
 };
 
 btn1.addEventListener('click',nextPag)
 btn2.addEventListener('click',nextPag)
-btn_retro.addEventListener('click',backPag)
+btn_back.addEventListener('click',backPag)
 btn_edit.addEventListener('click',backPag)
-
 btn_save.addEventListener('click',copiarPortapapeles)

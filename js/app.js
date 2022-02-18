3a3957392984650db4ef80db0276eed5de9bf51f
antac69 <angello_04_09@hotmail.com>
2022-02-18 12:19:32 -0500

    agregando funcion a btn-agregar

--- js/app.js
+++ js/app.js
@@ -1,50 +1,69 @@
 /* variables */
-const move_forms = document.getElementById('movimiento-forms');
-const move_btns = document.getElementById('movimiento-btns');
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const btn_back = document.getElementById('btn-retro');
-const btn_edit = document.getElementById('btn-editar');
-const btn_save = document.getElementById('btn-save');
-const report_area = document.getElementById('reporte');
-const items_container = document.querySelectorAll('.container-items');
+const move_forms = document.getElementById("movimiento-forms");
+const move_btns = document.getElementById("movimiento-btns");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn_back = document.getElementById("btn-retro");
+const btn_edit = document.getElementById("btn-editar");
+const btn_save = document.getElementById("btn-save");
+const report_area = document.getElementById("reporte");
+const items_container = document.querySelectorAll(".container-items");
 let pag = 0;
 
 /* funciones  reutilizables*/
-const limitNext =()=> pag <= 1 ? pag++ :  pag = 2;
-const limitBack =()=> pag > 0 ? pag-- :  pag = 0;
-const move =()=>{
-    move_forms.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
-    move_btns.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`
-}
-const nextPag=()=>{
-    limitNext()
-    move()
-}
-const backPag=()=>{
-    limitBack()
-    move()
-}
-const copiarPortapapeles =()=> {
-    report_area.select()
-    document.execCommand('copy')
-    btn_save.focus()
+const limitNext = () => (pag <= 1 ? pag++ : (pag = 2));
+const limitBack = () => (pag > 0 ? pag-- : (pag = 0));
+const move = () => {
+  move_forms.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`;
+  move_btns.style.transform = `translateX(calc(${pag} * (calc(-100% - 64px))))`;
+};
+const nextPag = () => {
+  limitNext();
+  move();
+};
+const backPag = () => {
+  limitBack();
+  move();
+};
+const copiarPortapapeles = () => {
+  report_area.select();
+  document.execCommand("copy");
+  btn_save.focus();
 };
 
 /* eventos */
-btn1.addEventListener('click',nextPag)
-btn2.addEventListener('click',nextPag)
-btn_back.addEventListener('click',backPag)
-btn_edit.addEventListener('click',backPag)
-btn_save.addEventListener('click',copiarPortapapeles)
-/* desplegar items */
-let prueba ;
-items_container.forEach(e=>{
-    e.addEventListener('click',(e)=>{
-        if (e.target.classList.contains('btn-desplegar')){
-            const header_item = e.target.parentElement;
-            header_item.classList.toggle('active');
-/*             console.log(header_item); */
-        }
-    })
-})
+btn1.addEventListener("click", nextPag);
+btn2.addEventListener("click", nextPag);
+btn_back.addEventListener("click", backPag);
+btn_edit.addEventListener("click", backPag);
+btn_save.addEventListener("click", copiarPortapapeles);
+/* aciones de botones items segun su id*/
+const acciones_items = {
+  "btn-desplegar": (btn) => {
+    const header_item = btn.parentElement;
+    header_item.classList.toggle("active");
+  },
+  "btn-agregar":(btn)=>{
+      /* ocultando boton agregar */
+      btn.classList.add('d-none')
+      /* constante de botones de accion */
+      const header_item_accion = btn.parentElement.querySelector('.acciones')
+      /* mostrando borones de accion */
+      header_item_accion.classList.remove('d-none')
+      /* definiendo cantidad */
+      const items_nd = header_item_accion.querySelector('#unidades')
+      items_nd.innerText = 1;
+      console.log(header_item_accion.classList)
+  }
+};
+items_container.forEach((e) => {
+  e.addEventListener("click", (e) => {
+    const btn = {
+      elemento: e.target,
+      name: e.target.id,
+    };
+    acciones_items[btn.name]
+      ? acciones_items[btn.name](btn.elemento)
+      : console.log(btn.name);
+  });
+});

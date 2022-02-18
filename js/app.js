763c471529beb1794836ad8c07a3e8ed916ac797
antac69 <angello_04_09@hotmail.com>
2022-02-18 16:14:12 -0500

    avanzando con postes, mufa aprecer y desaparecer btn-deplegar

--- js/app.js
+++ js/app.js
@@ -87,7 +87,6 @@ const acciones = {
 const cantidad_ferreteria = {
   "item-crucetas": (accion) => {
     const n = reporte.ferreteria.mufa.cantidad;
-    console.log(acciones["agregar"](n), 'soy reporte cantidad')
     reporte.ferreteria.mufa.cantidad = acciones[accion](n);
     return acciones[accion](n);
   },
@@ -110,54 +109,88 @@ const cantidad_ferreteria = {
 /* aciones de botones items segun su id*/
 const acciones_items = {
   "btn-desplegar": (btn) => {
-    const header_item = btn.parentElement;
-    header_item.classList.toggle("active");
+    const header_item_desplegar = btn.parentElement;
+    /* toggle para contraer y desplegar items */
+    header_item_desplegar.classList.toggle("active");
   },
   "btn-agregar": (btn) => {
-    /* ocultando boton agregar */
-    btn.classList.add("d-none");
     /* constante de botones de accion */
     const header_item_accion = btn.parentElement.querySelector(".acciones");
-    console.log(header_item_accion)
-    /* mostrando botones de accion */
-    header_item_accion.classList.remove("d-none");
     /* definiendo cantidad */
     const items_nd = header_item_accion.querySelector("#unidades");
     /* opteniendo id del item */
-    const item = header_item_accion.parentElement.parentElement.parentElement.id;
+    const item =
+      header_item_accion.parentElement.parentElement.parentElement.id;
+    /* ocultando boton agregar */
+    btn.classList.add("d-none");
+    /* mostrando botones de accion */
+    header_item_accion.classList.remove("d-none");
+    /* dando la cantidad de items */
     items_nd.innerText = cantidad_ferreteria[item]("agregar");
+    if(item == "item-crucetas" || item == "item-postes_instalados"){
+        const header_item_desplegar = btn.parentElement;
+        /* toggle para contraer y desplegar items */
+        header_item_desplegar.classList.add("active");
+        header_item_desplegar.parentElement.parentElement.querySelector('#btn-desplegar').classList.remove("d-none");
+        console.log('esta vacio')
+    }
   },
   "btn-aumentar": (btn) => {
     /* constante de botones de accion */
     const header_item_accion = btn.parentElement;
-    /* ocultando btn eliminar y mostrando boton disminuir */
     const btn_eliminar = header_item_accion.querySelector("#btn-eliminar");
-    btn_eliminar.classList.add("d-none");
     const btn_disminuir = header_item_accion.querySelector("#btn-disminuir");
-    btn_disminuir.classList.remove("d-none");
     /* definiendo cantidad */
     const items_nd = header_item_accion.querySelector("#unidades");
     /* opteniendo id del item */
-    const item = header_item_accion.parentElement.parentElement.parentElement.id;
+    const item =
+      header_item_accion.parentElement.parentElement.parentElement.id;
+    /* ocultando btn eliminar y mostrando boton disminuir */
+    btn_eliminar.classList.add("d-none");
+    btn_disminuir.classList.remove("d-none");
+    /* dando la cantidad de items */
     items_nd.innerText = cantidad_ferreteria[item]("agregar");
   },
   "btn-disminuir": (btn) => {
     const header_item_accion = btn.parentElement;
-    /* definiendo cantidad */
+    /* definiendo cantidad de items*/
     const items_nd = header_item_accion.querySelector("#unidades");
-    const item = header_item_accion.parentElement.parentElement.parentElement.id;
+    /* opteniendo id del item */
+    const item =
+      header_item_accion.parentElement.parentElement.parentElement.id;
+    /* constante de botones */
+    const btn_disminuir = header_item_accion.querySelector("#btn-disminuir");
+    const btn_eliminar = header_item_accion.querySelector("#btn-eliminar");
+    /* condicionales */
     if (parseInt(items_nd.innerText) >= 2) {
-        items_nd.innerText = cantidad_ferreteria[item]("eliminar");
-        if (parseInt(items_nd.innerText) == 1){
-            /* ocultando btn diminuir y mostrando boton eliminar */
-            const btn_disminuir = header_item_accion.querySelector("#btn-disminuir");
-            btn_disminuir.classList.add("d-none");
-            const btn_eliminar = header_item_accion.querySelector("#btn-eliminar");
-            btn_eliminar.classList.remove("d-none");
-            /* opteniendo id del item */
-        }
-    } 
-    console.log(parseInt(items_nd.innerText))
+      items_nd.innerText = cantidad_ferreteria[item]("eliminar");
+      if (parseInt(items_nd.innerText) == 1) {
+        /* ocultando btn diminuir y mostrando boton eliminar */
+        btn_disminuir.classList.add("d-none");
+        btn_eliminar.classList.remove("d-none");
+      }
+    }
+  },
+  "btn-eliminar": (btn) => {
+    /* constante de botones de accion */
+    const header_item_accion = btn.parentElement.parentElement.querySelector(".acciones");
+    /* opteniendo id del item */
+    const item = header_item_accion.parentElement.parentElement.parentElement.id;
+    console.log(header_item_accion,item)
+    /* definiendo cantidad de items*/
+    const items_nd = btn.parentElement.querySelector("#unidades");
+    items_nd.innerText = cantidad_ferreteria[item]("eliminar");
+    /* mostrando boton agregar */
+    header_item_accion.parentElement.querySelector("#btn-agregar").classList.remove("d-none");
+    /* mostrando botones de accion */
+    header_item_accion.classList.add("d-none");
+    if(item == "item-crucetas" || item == "item-postes_instalados"){
+        const header_item_desplegar = btn.parentElement;
+        /* toggle para contraer y desplegar items */
+        header_item_desplegar.classList.remove("active");
+        header_item_desplegar.parentElement.parentElement.querySelector('#btn-desplegar').classList.add("d-none");
+        console.log('esta vacio')
+    }
   },
 };
 
